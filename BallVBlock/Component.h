e058f1cfcdd37e96a50f2d42640055558add05a1
sfaulk7 <sfaulk.busy@gmail.com>
2025-01-08 17:00:27 -0600

    Almost finished Component Header

--- BallVBlock/Component.h
+++ BallVBlock/Component.h
@@ -1,6 +1,47 @@
 #pragma once
 
-class Component
+class Actor
 {
 
-}
+};
+
+class Component
+{
+public:
+	Actor* m_owner;
+	Actor* Owner;
+	bool Enabled;
+	bool Started;
+
+	Actor* GetOwner();
+	void SetOwner();
+	virtual void OnEnable()
+	{
+
+	}
+	virtual void OnDisable()
+	{
+
+	}
+
+	virtual void Start()
+	{
+
+	}
+	virtual void Update(double deltatime)
+	{
+		if (Owner = nullptr)
+		{
+			throw NullReferenceException();
+		}
+	}
+	virtual void End()
+	{
+
+	}
+
+private:
+	bool _enabled;
+	bool _started;
+
+};
