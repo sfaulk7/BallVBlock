aa5f25b3dcaeb3f176124c5d50bba540d133273c
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2025-01-15 09:58:38 -0600

    Started to work on the player

--- BallVBlock/Player.h
+++ BallVBlock/Player.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "raylib.h"
+
+class Actor;
+
+class Player
+{
+public:
+	float Speed = 50;
+	virtual void Update(double deltaTime);
+private:
+	Color m_color = Color(RED);
+protected:
+
+};
